0d6cfdcd10dbf1e27d41f2c25d7eb07d7944be4b
sukanta hui <huisukanta77@gmail.com>
2020-06-11 08:49:48 +0530

    test property added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
-
+  test: string;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
